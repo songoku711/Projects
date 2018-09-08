325b3833c49b80b07b3af9cdb37a3eb460ab10f4
Huy Long <43087878+songoku711@users.noreply.github.com>
2018-09-08 16:49:29 +0700

    Create PIC18F26K22 bootloader
    Create PIC18F26K22 bootloader

---
+++ MPLAB X/[18F26K22] Bootloader/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=_18F26K22__Bootloader.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/_18F26K22__Bootloader.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=[18f26k22]bootloader.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/[18f26k22]bootloader.tar
